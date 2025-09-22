d3bee1612b35dda66c10ca1c0461de296b659f47
Potato-Technical <74062786+Potato-Technical@users.noreply.github.com.>
2025-09-22 05:14:50 +0200

    feat(static): ajout pages Mentions légales, CGU et Accessibilité

---
+++ app/Views/static/cgu.php
@@ -0,0 +1,5 @@
+<div class="container my-5">
+  <h1>Conditions Générales d’Utilisation</h1>
+  <p>Cette page est en cours de construction.</p>
+  <a href="/" class="btn btn-primary">Retour à l'accueil</a>
+</div>
---
+++ app/Views/static/mentions.php
@@ -0,0 +1,5 @@
+<div class="container my-5">
+  <h1>Mentions légales</h1>
+  <p>Cette page est en cours de construction.</p>
+  <a href="/" class="btn btn-primary">Retour à l'accueil</a>
+</div>
